4c5993c88b8fecce650f667cf0527cde8fcb053e
Ильгам <ilgam.shaidullin@gmail.com>
2024-11-16 03:03:13 +0300

    Init dotnet console App

---
+++ uni/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hi, my little bro. What's your name?");
+Console.Write("My name is ");
+string? name = Console.ReadLine();
